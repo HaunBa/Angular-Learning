9343538c34f6d0a8d267780b178b6c5c863c7bff
HaunBa <haunschmied.b@gmail.com>
2022-10-27 15:04:14 +0200

    added AuthorizationAPI

---
+++ AuthorizationAPI/AuthorizationAPI/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace AuthorizationAPI
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddIdentityServer()
+                //.AddTestUsers(InMemoryConfig.GetUsers())
+                .AddDeveloperSigningCredential() //not something we want to use in a production environment
+                .AddConfigurationStore(opt =>
+                {
+                    opt.ConfigureDbContext = c => c.UseSqlServer(configRoot.GetConnectionString("DefaultConnection"),
+                        sql => sql.MigrationsAssembly(migrationAssembly));
+                })
+                .AddOperationalStore(opt =>
+                {
+                    opt.ConfigureDbContext = o => o.UseSqlServer(configRoot.GetConnectionString("DefaultConnection"),
+                        sql => sql.MigrationsAssembly(migrationAssembly));
+                });
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.Run();
+        }
+    }
+}
